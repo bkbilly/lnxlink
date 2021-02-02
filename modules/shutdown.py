c43b525a1232f790af078016b6a3516670e0dbe4
bkbilly <bkbilly@hotmail.com>
2021-02-02 19:19:57 +0200

    added shutdown

---
+++ modules/shutdown.py
@@ -0,0 +1,11 @@
+import subprocess
+
+class Addon():
+    service = 'shutdown'
+    name = 'Shutdown'
+    icon = 'mdi:power'
+    unit = 'json'
+
+    def startControl(self, topic, data):
+        print(topic, data)
+        subprocess.call(["shutdown", "-t", "10"])
