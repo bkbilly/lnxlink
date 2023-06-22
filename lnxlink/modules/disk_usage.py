dc2328cee52747831f4b8add1e0830449521d164
bkbilly <bkbilly@hotmail.com>
2023-06-22 18:28:34 +0300

    Create separate sensors for each disk Also fixes issue #60

--- lnxlink/modules/disk_usage.py
+++ lnxlink/modules/disk_usage.py
@@ -5,22 +5,39 @@ class Addon():
 
     def __init__(self, lnxlink):
         self.name = 'Disk Usage'
-        self.sensor_type = 'sensor'
-        self.icon = 'mdi:harddisk'
 
-    def getInfo(self) -> dict:
-        disks = {"status": False}
+    def exposedControls(self):
+        discovery_info = {}
         for disk in psutil.disk_partitions():
             if disk.fstype == 'squashfs':
                 continue
-            disks[disk.device] = {}
+            if 'docker/overlay' in disk.mountpoint:
+                continue
+            device = disk.device.replace('/', '_').strip('_')
+            discovery_info[f"Disk {device}"] = {
+                "type": "sensor",
+                "icon": "mdi:harddisk",
+                "unit": "%",
+                "state_class": "measurement",
+                "value_template": f"{{{{ value_json.{device}.percent }}}}",
+                "enabled": True,
+            }
+        return discovery_info
+
+    def getControlInfo(self):
+        disks = {}
+        for disk in psutil.disk_partitions():
+            if disk.fstype == 'squashfs':
+                continue
+            if 'docker/overlay' in disk.mountpoint:
+                continue
+            device = disk.device.replace('/', '_').strip('_')
+            disks[device] = {}
             disk_stats = psutil.disk_usage(disk.mountpoint)
-            disks[disk.device]["total"] = self._bytetomb(disk_stats.total)
-            disks[disk.device]["used"] = self._bytetomb(disk_stats.used)
-            disks[disk.device]["free"] = self._bytetomb(disk_stats.free)
-            disks[disk.device]["percent"] = disk_stats.percent
-            if disk_stats.percent > 80:
-                disks["status"] = True
+            disks[device]["total"] = self._bytetomb(disk_stats.total)
+            disks[device]["used"] = self._bytetomb(disk_stats.used)
+            disks[device]["free"] = self._bytetomb(disk_stats.free)
+            disks[device]["percent"] = disk_stats.percent
         return disks
 
     def _bytetomb(self, byte):
