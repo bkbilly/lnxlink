616de0144b5bdbc5d404f470297c04fd7d6e4e3f
bkbilly <bkbilly@hotmail.com>
2023-08-24 18:31:44 +0300

    Add power profile

---
+++ lnxlink/modules/power_profile.py
@@ -0,0 +1,40 @@
+"""Selects the Power Profile"""
+import re
+
+
+class Addon:
+    """Addon module"""
+
+    def __init__(self, lnxlink=None):
+        """Setup addon"""
+        self.name = "Power Profile"
+        self.lnxlink = lnxlink
+        self.options = self._get_power_profiles()
+
+    def get_info(self):
+        """Gather information from the system"""
+        stdout, _ = self.lnxlink.subprocess("powerprofilesctl get")
+        return stdout
+
+    def exposed_controls(self):
+        """Exposes to home assistant"""
+        return {
+            "Power Profile": {
+                "type": "select",
+                "icon": "mdi:harddisk-plus",
+                "options": self.options,
+            }
+        }
+
+    def start_control(self, topic, data):
+        """Control system"""
+        self.lnxlink.subprocess(f"powerprofilesctl set {data}")
+
+    def _get_power_profiles(self):
+        """Get the power profiles in the correct order"""
+        profiles_pattern = re.compile(r"([\w-]+):\n")
+
+        stdout, _ = self.lnxlink.subprocess("powerprofilesctl list")
+        profiles = re.findall(profiles_pattern, stdout)
+
+        return profiles
