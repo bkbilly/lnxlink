d54a544ee9c1a5bce642eda4387880513e74b0dc
bkbilly <bkbilly@hotmail.com>
2023-12-24 22:30:01 +0200

    Add display variable if missing for xdotool

--- lnxlink/modules/send_keys.py
+++ lnxlink/modules/send_keys.py
@@ -1,5 +1,9 @@
 """Uses xdotool to press keyboard keys"""
+import os
+import logging
 from .scripts.helpers import syscommand
+
+logger = logging.getLogger("lnxlink")
 
 
 class Addon:
@@ -8,6 +12,7 @@ class Addon:
     def __init__(self, lnxlink):
         """Setup addon"""
         self.name = "Send Keys"
+        self.lnxlink = lnxlink
 
     def exposed_controls(self):
         """Exposes to home assistant"""
@@ -20,4 +25,8 @@ class Addon:
 
     def start_control(self, topic, data):
         """Control system"""
+        if os.environ.get("DISPLAY") is None:
+            if self.lnxlink.display is not None:
+                os.environ["DISPLAY"] = self.lnxlink.display
+                logger.info("Initializing empty DISPLAY environment variable")
         syscommand(f"xdotool key {data}")
