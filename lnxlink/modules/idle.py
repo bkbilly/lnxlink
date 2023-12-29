6da53ecd28018a59c6d79340b201b6d752d599d0
bkbilly <bkbilly@hotmail.com>
2023-12-29 20:21:55 +0200

    Auto update dbus_idle to newest version

--- lnxlink/modules/idle.py
+++ lnxlink/modules/idle.py
@@ -1,6 +1,8 @@
 """Gets the idle time of the system"""
+import sys
 import logging
-from .scripts.helpers import import_install_package
+import importlib
+from .scripts.helpers import import_install_package, syscommand
 
 logger = logging.getLogger("lnxlink")
 
@@ -26,6 +28,20 @@ class Addon:
             idle_ms = self.lib["dbus_idle"].IdleMonitor().get_dbus_idle()
             idle_sec = round(idle_ms / 1000, 0)
             return idle_sec
+        except NotImplementedError:
+            logger.error("Unsupported version of dbus-idle found. Try to update...")
+            args = [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "-U",
+                "--quiet",
+                "dbus-idle",
+            ]
+            syscommand(args, timeout=None)
+            importlib.reload(self.lib["dbus_idle"])
+            return 0
         except Exception as err:
             logger.error(err)
             return 0
