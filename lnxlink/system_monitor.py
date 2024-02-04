0c7ad7e8ccf9a30607a40f90bfa52bef0161e752
bkbilly <bkbilly@hotmail.com>
2024-02-04 10:45:24 +0200

    Make DBUS optional

--- lnxlink/system_monitor.py
+++ lnxlink/system_monitor.py
@@ -5,9 +5,10 @@ import signal
 import logging
 import traceback
 
-import dbus
-from dbus.mainloop.glib import DBusGMainLoop
-from gi.repository import GLib
+# import dbus
+# from dbus.mainloop.glib import DBusGMainLoop
+# from gi.repository import GLib
+from .modules.scripts.helpers import import_install_package
 
 logger = logging.getLogger("lnxlink")
 
@@ -17,9 +18,14 @@ class MonitorSuspend:
 
     def __init__(self, callback):
         self.use = False
+        self._requirements()
         try:
-            dbus_loop = DBusGMainLoop(set_as_default=True)  # integrate into main loob
-            bus = dbus.SystemBus(mainloop=dbus_loop)  # connect to dbus system wide
+            dbus_loop = self.lib["dbus-loop"].mainloop.glib.DBusGMainLoop(
+                set_as_default=True
+            )  # integrate into main loob
+            bus = self.lib["dbus"].SystemBus(
+                mainloop=dbus_loop
+            )  # connect to dbus system wide
             bus.add_signal_receiver(  # defince the signal to listen to
                 callback,  # name of callback function
                 "PrepareForSleep",  # singal name
@@ -32,7 +38,7 @@ class MonitorSuspend:
                 "org.freedesktop.login1.Manager",  # interface
                 "org.freedesktop.login1",  # bus name
             )
-            self.loop = GLib.MainLoop()
+            self.loop = self.lib["glib"].repository.GLib.MainLoop()
             self.timer1 = threading.Thread(target=self.loop.run)
             self.use = True
         except Exception as err:
@@ -41,6 +47,18 @@ class MonitorSuspend:
                 err,
                 traceback.format_exc(),
             )
+
+    def _requirements(self):
+        self.lib = {
+            "dbus-loop": import_install_package(
+                "dbus-python", ">=1.3.2", "dbus.mainloop.glib"
+            ),
+            "dbus": import_install_package("dbus-python", ">=1.3.2", "dbus"),
+            "cairo": import_install_package("pycairo", ">=1.24.0", "cairo"),
+            "glib": import_install_package(
+                "PyGObject", ">=3.44.0", "gi.repository.GLib"
+            ),
+        }
 
     def start(self):
         """Start the timer of the thread"""
