c100c552cbfaab6539ee123d8dab5910824246f9
Vasilis Koulis <bkbilly@hotmail.com>
2024-02-11 01:31:34 +0200

    Add pacman & dnf update check and check if package manager is supported

--- lnxlink/modules/sys_updates.py
+++ lnxlink/modules/sys_updates.py
@@ -1,5 +1,6 @@
 """Checks for system updates"""
 import time
+from shutil import which
 from .scripts.helpers import syscommand
 
 
@@ -12,6 +13,29 @@ class Addon:
         self.last_time = 0
         self.update_interval = 360  # Check for updates every 6 minutes
         self.update_available = False
+        self.package_manager = None
+        if which("apt") is not None:
+            self.package_manager = {
+                "command": "apt list --upgradable | wc -l",
+                "largerthan": 2,
+            }
+        elif which("yum") is not None:
+            self.package_manager = {
+                "command": "yum -q updateinfo list updates | wc -l",
+                "largerthan": 2,
+            }
+        elif which("pacman") is not None:
+            self.package_manager = {
+                "command": "pacman -Qu | wc -l",
+                "largerthan": 0,
+            }
+        elif which("dnf") is not None:
+            self.package_manager = {
+                "command": "dnf updateinfo list | wc -l",
+                "largerthan": 1,
+            }
+        else:
+            raise SystemError("System commands not found for package manager")
 
     def exposed_controls(self):
         """Exposes to home assistant"""
@@ -25,23 +49,9 @@ class Addon:
 
     def get_info(self):
         """Gather information from the system"""
-        packages = [
-            {
-                "command": "apt list --upgradable | wc -l",
-                "logic": "> 2",
-            },
-            {
-                "command": "yum -q updateinfo list updates | wc -l",
-                "logic": "> 2",
-            },
-        ]
-
         cur_time = time.time()
         if cur_time - self.last_time > self.update_interval:
             self.last_time = cur_time
-            for package in packages:
-                stdout, _, _ = syscommand(package["command"])
-                self.update_available = eval(f"{stdout}{package['logic']}")
-                if self.update_available:
-                    return True
+            stdout, _, _ = syscommand(self.package_manager["command"])
+            self.update_available = int(stdout) > self.package_manager["largerthan"]
         return self.update_available
