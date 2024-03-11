46a7e4956ab4dbf16ff50ca36e5d0a9c8ca66ce4
Phillip <p_w999@ymail.com>
2024-03-11 10:49:19 +0100

    Fix linting

--- lnxlink/modules/disk_usage.py
+++ lnxlink/modules/disk_usage.py
@@ -52,8 +52,16 @@ class Addon:
     def _get_disks(self):
         """Get a list of all disks"""
         disks = {}
-        disk_includes = self.lnxlink.config["settings"].get("disk_usage", []).get("include_disks", [])
-        disk_excludes = self.lnxlink.config["settings"].get("disk_usage", []).get("exclude_disks", [])
+        disk_includes = (
+            self.lnxlink.config["settings"]
+            .get("disk_usage", [])
+            .get("include_disks", [])
+        )
+        disk_excludes = (
+            self.lnxlink.config["settings"]
+            .get("disk_usage", [])
+            .get("exclude_disks", [])
+        )
         for disk in psutil.disk_partitions():
             matches = [
                 "/snap/",
