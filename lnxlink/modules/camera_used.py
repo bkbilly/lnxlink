c7ff8721a81efd6d1d9738c3794d6a80c2fa5569
bkbilly <bkbilly@hotmail.com>
2024-07-20 19:45:20 +0300

    Asynchronous get status of camera_used

--- lnxlink/modules/camera_used.py
+++ lnxlink/modules/camera_used.py
@@ -1,4 +1,7 @@
 """Checks if the webcam is used"""
+import glob
+from threading import Thread
+from inotify.adapters import Inotify
 from lnxlink.modules.scripts.helpers import syscommand
 
 
@@ -8,15 +11,26 @@ class Addon:
     def __init__(self, lnxlink):
         """Setup addon"""
         self.name = "Camera used"
+        self.lnxlink = lnxlink
+        self.inotify = Inotify()
+        self.cameras = []
+        Thread(target=self._watch_events, daemon=True).start()
 
     def get_info(self):
         """Gather information from the system"""
-        _, _, returncode = syscommand("fuser /dev/video*", ignore_errors=True)
-        cam_used = False
-        if returncode == 0:
-            cam_used = True
+        cameras = glob.glob("/dev/video*", recursive=True)
+        if cameras != self.cameras:
+            self.cameras = cameras
+            for camera in cameras:
+                self.inotify.add_watch(camera)
 
-        return cam_used
+    def _watch_events(self):
+        for event in self.inotify.event_gen(yield_nones=False):
+            _, _, returncode = syscommand(f"fuser {event[2]}", ignore_errors=True)
+            cam_used = False
+            if returncode == 0:
+                cam_used = True
+            self.lnxlink.run_module(self.name, cam_used)
 
     def exposed_controls(self):
         """Exposes to home assistant"""
