a83ac1e160256913bf325f3aa04987d1003b57a4
bkbilly <bkbilly@hotmail.com>
2024-08-17 02:13:27 +0300

    Check if commands exist before running

--- lnxlink/modules/keep_alive.py
+++ lnxlink/modules/keep_alive.py
@@ -29,48 +29,59 @@ class Addon:
         enabled_list = []
 
         # Check if Gnome Idle Time is active
-        stdout_dim, _, _ = syscommand(
-            "gsettings get org.gnome.desktop.session idle-delay"
-        )
-        stdout_suspend, _, _ = syscommand(
-            "gsettings get org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type"
-        )
-        if stdout_dim != "":
-            disabled_dim = "0" == stdout_dim.split()[1]
-            if disabled_dim and stdout_suspend != "":
-                enabled_list.append("nothing" in stdout_suspend)
+        if which("gsettings"):
+            stdout_dim, _, _ = syscommand(
+                "gsettings get org.gnome.desktop.session idle-delay"
+            )
+            stdout_suspend, _, _ = syscommand(
+                "gsettings get org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type"
+            )
+            if stdout_dim != "":
+                disabled_dim = "0" == stdout_dim.split()[1]
+                if disabled_dim and stdout_suspend != "":
+                    enabled_list.append("nothing" in stdout_suspend)
 
         # Check if DPMS is active
-        if self.lnxlink.display is not None:
-            stdout_xset, _, _ = syscommand(f"xset -display {self.lnxlink.display} q")
-            xset_pattern = re.compile(r"Standby: (\d+)\s+Suspend: (\d+)\s+Off: (\d+)")
-            xset_match = re.findall(xset_pattern, stdout_xset)
-            for nums in xset_match:
-                enabled_list.append(all(num != "0" for num in nums))
-                if enabled_list[-1]:
-                    enabled_list.append("DPMS is Enabled" in stdout_xset)
+        if which("xset"):
+            if self.lnxlink.display is not None:
+                stdout_xset, _, _ = syscommand(
+                    f"xset -display {self.lnxlink.display} q"
+                )
+                xset_pattern = re.compile(
+                    r"Standby: (\d+)\s+Suspend: (\d+)\s+Off: (\d+)"
+                )
+                xset_match = re.findall(xset_pattern, stdout_xset)
+                for nums in xset_match:
+                    enabled_list.append(all(num != "0" for num in nums))
+                    if enabled_list[-1]:
+                        enabled_list.append("DPMS is Enabled" in stdout_xset)
 
         return any(enabled_list)
 
     def start_control(self, topic, data):
         """Control system"""
         if data.lower() == "off":
-            syscommand(
-                "gsettings set org.gnome.desktop.session idle-delay 600",
-            )
-            syscommand(
-                'gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "suspend"',
-            )
-            syscommand(
-                "xset +dpms",
-            )
+            if which("gsettings"):
+                syscommand(
+                    "gsettings set org.gnome.desktop.session idle-delay 600",
+                )
+                syscommand(
+                    'gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "suspend"',  # noqa: E501
+                )
+            if which("xset"):
+                syscommand(
+                    "xset +dpms",
+                )
         elif data.lower() == "on":
-            syscommand(
-                "gsettings set org.gnome.desktop.session idle-delay 0",
-            )
-            syscommand(
-                'gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "nothing"',
-            )
-            syscommand(
-                "xset -dpms",
-            )
+            if which("gsettings"):
+                syscommand(
+                    "gsettings set org.gnome.desktop.session idle-delay 0",
+                )
+
+                syscommand(
+                    'gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "nothing"',  # noqa: E501
+                )
+            if which("xset"):
+                syscommand(
+                    "xset -dpms",
+                )
