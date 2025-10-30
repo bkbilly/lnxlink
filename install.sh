c3de73a347c2434b6706076889a72bb47006a841
bkbilly <bkbilly@hotmail.com>
2025-10-30 21:26:41 +0200

    add NixOS commands

--- install.sh
+++ install.sh
@@ -1,6 +1,52 @@
 #!/bin/bash
 
 
+find_lnxlink_binary_path() {
+    local binary_name="lnxlink"
+
+    # 1. Check in $PATH using command -v
+    if command -v "$binary_name" >/dev/null 2>&1; then
+        local path_in_path=$(command -v "$binary_name")
+        echo "$path_in_path"
+        return 0 # Success
+    fi
+
+    # 2. Check in $HOME/.local/bin/
+    local path_local="$HOME/.local/bin/$binary_name"
+    if [[ -x "$path_local" ]]; then
+        echo "$path_local"
+        return 0 # Success
+    fi
+
+    # 3. Check in $HOME/bin/
+    local path_home_bin="$HOME/bin/$binary_name"
+    if [[ -x "$path_home_bin" ]]; then
+        echo "$path_home_bin"
+        return 0 # Success
+    fi
+
+    # 4. Check in $PIPX_BIN_DIR (If pipx is used and environment variable is set)
+    if [[ -n "$PIPX_BIN_DIR" ]]; then
+        local path_pipx="$PIPX_BIN_DIR/$binary_name"
+        if [[ -x "$path_pipx" ]]; then
+            echo "$path_pipx"
+            return 0 # Success
+        fi
+    fi
+
+    # 5. Root User Local Bin (The sudo/root default) ---
+    local path_root_local="/root/.local/bin/$binary_name"
+    if [[ -x "$path_root_local" ]]; then
+        echo "$path_root_local"
+        return 0 # Success
+    fi
+
+    # If not found, echo nothing and return failure
+    return 1 # Failure
+}
+
+
+
 # Global
 if [ -f /etc/debian_version ]; then
     installcommand='apt-get --yes install'
@@ -50,108 +96,56 @@ else
 fi
 
 
-# Installing system packages
-if ! command -v python3 >/dev/null 2>&1; then
-    echo -e "\e[31mPython3 not found, installing from $system package manager:\e[0m"
-    sudo $installcommand python3
-fi
-if ! command -v pipx >/dev/null 2>&1; then
-    echo -e "\e[31mPIPX not found, installing from $system package manager:\e[0m"
-    if [ "$system" == "arch/manjaro" ]; then
-        sudo $installcommand python-pipx
-    else
-        sudo $installcommand pipx
+if [ "$system" != "nixos" ]; then
+    # Installing system packages
+    if ! command -v python3 >/dev/null 2>&1; then
+        echo -e "\e[31mPython3 not found, installing from $system package manager:\e[0m"
+        sudo $installcommand python3
     fi
-    pipx ensurepath
-fi
-
-
-echo -e "\e[35mInstalling system dependencies...\e[0m"
-sudo $installcommand gcc cmake
-if [ "$system" == "debian/ubuntu" ]; then
-    sudo $installcommand python3-dev libasound2-dev portaudio19-dev python3-pyaudio
-elif [ "$system" == "redhat/fedora" ]; then
-    sudo $installcommand kernel-headers python3-devel alsa-lib-devel portaudio-devel python3-pyaudio
-elif [ "$system" == "alpine" ]; then
-    sudo $installcommand linux-headers python3-dev musl-dev
-elif [ "$system" == "arch/manjaro" ]; then
-    sudo $installcommand linux-headers python-pyaudio portaudio python-pyaudio
-elif [ "$system" == "suse/opensuse" ]; then
-    sudo $installcommand python3-devel gcc-c++ make
-elif [ "$system" == "gentoo" ]; then
-    sudo $installcommand dev-lang/python dev-python/pip dev-util/cmake
-elif [ "$system" == "void" ]; then
-    sudo $installcommand python3-devel base-devel
-elif [ "$system" == "nixos" ]; then
-    echo -e "\e[33mNote: You must add packages via your Nix config.\e[0m"
-    echo "  environment.systemPackages = with pkgs; ["
-    echo "    python313Packages.pip"
-    echo "    pipx"
-    echo "    bluez"
-    echo "    xdotool"
-    echo "    ethtool"
-    echo "    busybox"
-    echo "  ];"
-fi
-
-
-echo -e "\e[35mInstalling extra packages...\e[0m"
-sudo $installcommand xdotool xdg-utils swayidle
-
-# Manual Steps
-if [ "$system" == "alpine" ]; then
-    echo -e "\e[31mManual steps needed for alpine...\e[0m"
-    echo "# Edit the file: ~/.local/share/pipx/venvs/lnxlink/pyvenv.cfg"
-    echo "# Set the value from false to true, to import also system packages, that possibly were installed after venv creation:"
-    echo "include-system-site-packages = true"
-    echo "Run this command:"
-    echo "pipx inject lnxlink dbus-idle"
-fi
-
-
-find_lnxlink_binary_path() {
-    local binary_name="lnxlink"
-
-    # 1. Check in $PATH using command -v
-    if command -v "$binary_name" >/dev/null 2>&1; then
-        local path_in_path=$(command -v "$binary_name")
-        echo "$path_in_path"
-        return 0 # Success
-    fi
-
-    # 2. Check in $HOME/.local/bin/
-    local path_local="$HOME/.local/bin/$binary_name"
-    if [[ -x "$path_local" ]]; then
-        echo "$path_local"
-        return 0 # Success
-    fi
-
-    # 3. Check in $HOME/bin/
-    local path_home_bin="$HOME/bin/$binary_name"
-    if [[ -x "$path_home_bin" ]]; then
-        echo "$path_home_bin"
-        return 0 # Success
-    fi
-
-    # 4. Check in $PIPX_BIN_DIR (If pipx is used and environment variable is set)
-    if [[ -n "$PIPX_BIN_DIR" ]]; then
-        local path_pipx="$PIPX_BIN_DIR/$binary_name"
-        if [[ -x "$path_pipx" ]]; then
-            echo "$path_pipx"
-            return 0 # Success
+    if ! command -v pipx >/dev/null 2>&1; then
+        echo -e "\e[31mPIPX not found, installing from $system package manager:\e[0m"
+        if [ "$system" == "arch/manjaro" ]; then
+            sudo $installcommand python-pipx
+            pipx ensurepath
+        else
+            sudo $installcommand pipx
+            pipx ensurepath
         fi
     fi
 
-    # 5. Root User Local Bin (The sudo/root default) ---
-    local path_root_local="/root/.local/bin/$binary_name"
-    if [[ -x "$path_root_local" ]]; then
-        echo "$path_root_local"
-        return 0 # Success
+
+    echo -e "\e[35mInstalling system dependencies...\e[0m"
+    sudo $installcommand gcc cmake
+    if [ "$system" == "debian/ubuntu" ]; then
+        sudo $installcommand python3-dev libasound2-dev portaudio19-dev python3-pyaudio
+    elif [ "$system" == "redhat/fedora" ]; then
+        sudo $installcommand kernel-headers python3-devel alsa-lib-devel portaudio-devel python3-pyaudio
+    elif [ "$system" == "alpine" ]; then
+        sudo $installcommand linux-headers python3-dev musl-dev
+    elif [ "$system" == "arch/manjaro" ]; then
+        sudo $installcommand linux-headers python-pyaudio portaudio python-pyaudio
+    elif [ "$system" == "suse/opensuse" ]; then
+        sudo $installcommand python3-devel gcc-c++ make
+    elif [ "$system" == "gentoo" ]; then
+        sudo $installcommand dev-lang/python dev-python/pip dev-util/cmake
+    elif [ "$system" == "void" ]; then
+        sudo $installcommand python3-devel base-devel
     fi
 
-    # If not found, echo nothing and return failure
-    return 1 # Failure
-}
+
+    echo -e "\e[35mInstalling extra packages...\e[0m"
+    sudo $installcommand xdotool xdg-utils swayidle
+
+    # Manual Steps
+    if [ "$system" == "alpine" ]; then
+        echo -e "\e[31mManual steps needed for alpine...\e[0m"
+        echo "# Edit the file: ~/.local/share/pipx/venvs/lnxlink/pyvenv.cfg"
+        echo "# Set the value from false to true, to import also system packages, that possibly were installed after venv creation:"
+        echo "include-system-site-packages = true"
+        echo "Run this command:"
+        echo "pipx inject lnxlink dbus-idle"
+    fi
+fi
 
 
 # Install LNXlink
@@ -161,11 +155,21 @@ LNX_CONFIG_PATH=""
 
 if [ "$LNX_PATH" != "" ] && [ "$LNX_EXIT_STATUS" -eq 0 ]; then
     echo -e "\e[31mUpgrading LNXlink...\e[0m"
-    pipx upgrade lnxlink
+    if [ "$system" == "nixos" ]; then
+        nix-shell -p python311Packages.pipx --run "pipx upgrade lnxlink"
+    else
+        pipx upgrade lnxlink
+    fi
+
 else
     echo -e "\e[35mInstalling LNXlink...\e[0m"
-    pip install -U pipx
-    pipx install lnxlink
+    if [ "$system" == "nixos" ]; then
+        nix-shell -p python311Packages.pipx --run "pipx install lnxlink"
+    else
+        pip install -U pipx
+        pipx install lnxlink
+    fi
+    source ~/.bashrc
     LNX_PATH=$(find_lnxlink_binary_path)
     LNX_EXIT_STATUS=$?
     if [ "$LNX_PATH" == "" ] || [ "$LNX_EXIT_STATUS" -ne 0 ]; then
