59e8237398f6fd5951896442e901a85bff822a28
bkbilly <bkbilly@hotmail.com>
2025-12-24 13:28:25 +0200

    Add module for watching configuration changes

---
+++ lnxlink/modules/watch_changes.py
@@ -0,0 +1,40 @@
+"""Watches for configuration changes and restarts LNXlink"""
+import logging
+import hashlib
+import os
+
+logger = logging.getLogger("lnxlink")
+
+
+class Addon:
+    """Addon module"""
+
+    def __init__(self, lnxlink):
+        """Setup addon"""
+        self.name = "LNXlink update"
+        self.lnxlink = lnxlink
+        self.last_time = 0
+        self.last_updated = os.path.getmtime(self.lnxlink.config_path)
+        self.last_hash = self._get_file_hash(self.lnxlink.config_path)
+
+    def get_info(self):
+        """Gather information from the system"""
+        current_time = os.path.getmtime(self.lnxlink.config_path)
+        if current_time != self.last_updated:
+            self.last_updated = current_time
+            current_hash = self._get_file_hash(self.lnxlink.config_path)
+            if current_hash != self.last_hash:
+                self.last_hash = current_hash
+                self.lnxlink.restart_script()
+
+    def _get_file_hash(self, filepath):
+        """Generates a SHA-256 hash of the file content."""
+        hasher = hashlib.sha256()
+        try:
+            with open(filepath, "rb") as f:
+                # Read in chunks to handle large files efficiently
+                while chunk := f.read(8192):
+                    hasher.update(chunk)
+            return hasher.hexdigest()
+        except FileNotFoundError:
+            return None
