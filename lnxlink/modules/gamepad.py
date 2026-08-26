ba2939f67eac9729b4db8d06b6095863baa01ddc
Darafei Praliaskouski <me@komzpa.net>
2026-08-27 03:11:20 +0400

    Clear gamepad activity on disconnect

--- lnxlink/modules/gamepad.py
+++ lnxlink/modules/gamepad.py
@@ -3,7 +3,7 @@ import logging
 import re
 import struct
 import time
-from threading import Thread
+from threading import Lock, Thread
 
 from lnxlink.modules.scripts.helpers import syscommand
 
@@ -17,7 +17,9 @@ class Addon:
         self.name = "Gamepad"
         self.gamepads = []
         self.running_threads = {}
-        self.last_used = 0
+        self.last_used = {}
+        self.watcher_tokens = {}
+        self.activity_lock = Lock()
         self.timeout_used = 40
 
     def exposed_controls(self):
@@ -32,9 +34,10 @@ class Addon:
     def get_info(self):
         """Gather information from the system"""
         self.watch_gamepads()
-        if int(time.time()) - self.last_used < self.timeout_used:
-            return True
-        return False
+        now = int(time.time())
+        with self.activity_lock:
+            last_used = list(self.last_used.values())
+        return any(now - used < self.timeout_used for used in last_used)
 
     def watch_gamepads(self):
         """Watch for gamepad connections"""
@@ -48,16 +51,26 @@ class Addon:
 
             for event in set(self.running_threads) - set(match):
                 self.running_threads.pop(event)
+                with self.activity_lock:
+                    self.watcher_tokens.pop(event, None)
+                    self.last_used.pop(event, None)
 
         for event in match:
             running_thread = self.running_threads.get(event)
             if running_thread is None or not running_thread.is_alive():
-                watch_thr = Thread(target=self.watch_input, args=(event,), daemon=True)
+                watcher_token = object()
+                with self.activity_lock:
+                    self.watcher_tokens[event] = watcher_token
+                watch_thr = Thread(
+                    target=self.watch_input,
+                    args=(event, watcher_token),
+                    daemon=True,
+                )
                 watch_thr.start()
                 logger.debug("Started for: %s", event)
                 self.running_threads[event] = watch_thr
 
-    def watch_input(self, event):
+    def watch_input(self, event, watcher_token):
         """Thread that watches gamepad inputs"""
         decode_str = "llHHI"
         try:
@@ -65,7 +78,9 @@ class Addon:
                 while game_data := file.read(struct.calcsize(decode_str)):
                     _, _, ev_type, code, value = struct.unpack(decode_str, game_data)
                     if ev_type != 0 or code != 0 or value != 0:
-                        self.last_used = int(time.time())
+                        with self.activity_lock:
+                            if self.watcher_tokens.get(event) is watcher_token:
+                                self.last_used[event] = int(time.time())
                         logger.debug("%s %s", code, value)
         except OSError as err:
             # Errno 19: "No such device" happens when the gamepad is disconnected
@@ -75,3 +90,8 @@ class Addon:
                 logger.error("Gamepad error for %s: %s", event, err)
         except Exception as err:
             logger.error("Unexpected error for %s: %s", event, err)
+        finally:
+            with self.activity_lock:
+                if self.watcher_tokens.get(event) is watcher_token:
+                    self.watcher_tokens.pop(event, None)
+                    self.last_used.pop(event, None)
